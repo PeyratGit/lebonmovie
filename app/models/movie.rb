ec53f70f060a922c33e05b8c4c3607b64324c806
Sidney Ouarzazi <sidney.o@gmail.com>
2022-02-21 17:01:43 +0100

    Added description and movie director and actor id optional

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  belongs_to :movie_director
-  belongs_to :movie_actor
+  belongs_to :movie_director, optional: true
+  belongs_to :movie_actor, optional: true
   belongs_to :user
   validates :title, presence: true
   validates :description, length: { minimum: 10 }

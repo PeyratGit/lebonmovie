a51d7e841aef894302aa295ea84cdeeccd9c753f
Clement Peyrat <aiglefeu31@gmail.com>
2022-02-22 11:18:03 +0100

    Created movies and purchases controllers. Added all routes for both controllers

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,25 @@
+class PurchasesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def mark_as_received
+  end
+
+  def accept_request
+  end
+
+  def decline_request
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :movies
+  resources :purchases, except: [:delete, :edit]
+  patch '/purchases/:id', to: 'purchases#mark_as_received'
+  patch '/purchases/:id', to: 'purchases#accept_request'
+  patch '/purchases/:id', to: 'purchases#decline_request'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

3df2a83bbac133548c9075e00b7bd898fbc12e19
Clement Peyrat <aiglefeu31@gmail.com>
2022-02-22 11:46:33 +0100

    Modified purchases routes and controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -11,15 +11,10 @@ class PurchasesController < ApplicationController
   def create
   end
 
+  def edit
+  end
+
   def update
   end
 
-  def mark_as_received
-  end
-
-  def accept_request
-  end
-
-  def decline_request
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :movies
-  resources :purchases, except: [:delete, :edit]
-  patch '/purchases/:id', to: 'purchases#mark_as_received'
-  patch '/purchases/:id', to: 'purchases#accept_request'
-  patch '/purchases/:id', to: 'purchases#decline_request'
+  resources :movies do
+    resources :purchases, only: [:new, :create]
+  end
+  resources :purchases, except: [:delete, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

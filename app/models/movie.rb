1fcb993a0073eb2d387b1b8b0a75ee7e147358aa
Canelle Deguitre <cdeguitre@outlook.fr>
2022-02-24 17:38:36 +0100

    design buttons + zoom

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :movie_director, optional: true
   belongs_to :movie_actor, optional: true
   belongs_to :user
+  has_many :purchases, dependent: :destroy
   has_one_attached :photo
   validates :title, :description, presence: true
   validates :description, length: { minimum: 10 }
